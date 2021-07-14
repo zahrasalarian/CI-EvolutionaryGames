a576140f4085b382adc57ed4e7fe78d3ada74eac
amberbrown4 <zasalarian2000@gmail.com>
2021-07-14 13:48:27 +0430

    Completed nn.py

---
+++ nn.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class NeuralNetwork():
+
+    def __init__(self, layer_sizes):
+
+        # TODO
+        # layer_sizes example: [4, 10, 2]
+        self.inp_llen = layer_sizes[0]
+        self.hidden_llen = layer_sizes[1]
+        self.outp_llen = layer_sizes[2]
+        self.b = [np.zeros(self.hidden_llen), np.zeros(self.outp_llen)] 
+        self.w = [np.random.randn(self.hidden_llen, self.inp_llen), np.random.randn(self.outp_llen, self.hidden_llen)]
+
+    def activation(self, x):
+        
+        # TODO
+        s = 1/(1+np.exp(-x))
+        return s
+
+    def forward(self, x):
+        
+        # TODO
+        # x example: np.array([[0.1], [0.2], [0.3]])
+        w_layer_2 = self.w[0]
+        b_2 = self.b[0]
+        
+        w_layer_3 = self.w[1]
+        b_3 = self.b[1]
+
+        a_s = []
+        # hidden layer
+        a_2 = np.zeros((self.hidden_llen, 1))
+        a_2, _ = self.activation(w_layer_2 @ x + b_2)
+        a_s.append(np.transpose(a_2).reshape(self.hidden_llen,1))
+
+        # output layer 
+        a_3 = np.zeros((self.outp_llen, 1))
+        a_3, _ = self.activation(w_layer_3 @ a_2 + b_3)
+        a_s.append(np.transpose(a_3).reshape(self.outp_llen,1))
+
+        return a_3
